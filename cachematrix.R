61e87ff741f6013edb6dcc32ae7d2dc76d19a62c
Pankaj Bhatlawande <pankaj07.coep@gmail.com>
2014-09-22 02:28:48 +0530

    Code to Calculate Inverse of Matrix
    Added 2 functions which calculate inverse of matrix

---
+++ cachematrix.R
@@ -0,0 +1,16 @@
+## There are two functions below
+#makeCacheMatrix - for creating a matrix
+#cacheSolve - to calculate the inverse of matrix returned by makeCacheMatrix
+
+## Function to create new matrix
+
+makeCacheMatrix <- function(x = matrix()) {
+
+}
+
+
+## Function to Calculate reverse of the matrix
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+}
